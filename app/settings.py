e02cb690f01eff0b5bf694a64a3025e31fa28503
makufarmerlyn <maku@farmerline.co>
2025-05-04 20:26:43 +0000

    Serving Media Files (Fixing No PDF file or URL provided):

--- app/settings.py
+++ app/settings.py
@@ -255,6 +255,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Optional: for custom static files
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
